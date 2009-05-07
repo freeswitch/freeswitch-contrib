544f077e4664948baa1ed9435a5d48489e2e0a9c
Ken Rice <krice@freeswitch.org>
2009-05-07 16:40:37 +0000

    ooops
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/scripts/contrib@13252 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- swk/php/esl_cdr/esl_cdr.php
+++ swk/php/esl_cdr/esl_cdr.php
@@ -58,49 +58,50 @@ for(;;) {
 			}
 		}
 
-        $insert = sprintf("INSERT into cdr (uuid, acctcode, ani, e164_ani, dnis, translated_dnis, e164_dnis, src_host, read_codec, write_codec, clid_name, ".
-	"clid_number, src_sdp, dest_sdp, originate_disposition, bridge_to_uuid, endpoint_disposition, sip_hangup_disposition, term_cause, ".
-	"hangup_cause, start_stamp, answer_stamp, progress_media_stamp, end_stamp, duration, billsec, progress_mediasec, billmin, ".
-	"carrier_id, cust_cost, carrier_cost)".
-	"VALUES".
-	"('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s', '%s',".
-	"%s, %s, %s, %s, %s, %s, %s)",
-	$data['Unique-ID'],
-	$data['acctcode'],
-	$data['Caller-Caller-ID-Number'],
-	$data['Caller-Caller-ID-Number'],
-	urldecode($data['Caller-Destination-Number']),
-	urldecode($data['Caller-Destination-Number']),
-	urldecode($data['Caller-Destination-Number']),
-	$data['Caller-Network-Addr'],
-	$data['Channel-Read-Codec-Name']."-". $data['Channel-Read-Codec-Rate'],
-	$data['Channel-Write-Codec-Name']."-". $data['Channel-Write-Codec-Rate'],
-	urlencode($data['Caller-Caller-ID-Name']),
-	$data['Caller-Caller-ID-Number'],
-	$data['variable_switch_r_sdp'],
-	$data['variable_switch_m_sdp'],
-	$data['variable_originate_disposition'],
-	$data['variable_bridge_to'],
-	$data['variable_endpoint_disposition'],
-	$data['variable_sip_hangup_disposition'],
-	$data['variable_sip_term_cause'],
-	$data['variable_hangup_cause'],
-	urldecode($data['variable_start_stamp']) . '-05',
-	isset($data['variable_answer_stamp'])?'\''. urldecode($data['variable_answer_stamp']) . '-05\'':'null',
-	urldecode($data['variable_progress_media_stamp']) . '-05',
-	urldecode($data['variable_end_stamp']) . '-05',
-	$data['variable_duration'],
-	$data['variable_billsec'],
-	$data['variable_progress_mediasec'],
-	$data['variable_billsec'],
-	"0",
-	"0",
-	"0"
-	);
-
-	$inserted = $dbh->exec($insert);
-	if ($inserted < 1){
-		die($db->errorInfo());
+        	$insert = sprintf("INSERT into cdr (uuid, acctcode, ani, e164_ani, dnis, translated_dnis, e164_dnis, src_host, read_codec, write_codec, clid_name, ".
+		"clid_number, src_sdp, dest_sdp, originate_disposition, bridge_to_uuid, endpoint_disposition, sip_hangup_disposition, term_cause, ".
+		"hangup_cause, start_stamp, answer_stamp, progress_media_stamp, end_stamp, duration, billsec, progress_mediasec, billmin, ".
+		"carrier_id, cust_cost, carrier_cost)".
+		"VALUES".
+		"('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s', '%s',".
+		"%s, %s, %s, %s, %s, %s, %s)",
+		$data['Unique-ID'],
+		$data['acctcode'],
+		$data['Caller-Caller-ID-Number'],
+		$data['Caller-Caller-ID-Number'],
+		urldecode($data['Caller-Destination-Number']),
+		urldecode($data['Caller-Destination-Number']),
+		urldecode($data['Caller-Destination-Number']),
+		$data['Caller-Network-Addr'],
+		$data['Channel-Read-Codec-Name']."-". $data['Channel-Read-Codec-Rate'],
+		$data['Channel-Write-Codec-Name']."-". $data['Channel-Write-Codec-Rate'],
+		urlencode($data['Caller-Caller-ID-Name']),
+		$data['Caller-Caller-ID-Number'],
+		$data['variable_switch_r_sdp'],
+		$data['variable_switch_m_sdp'],
+		$data['variable_originate_disposition'],
+		$data['variable_bridge_to'],
+		$data['variable_endpoint_disposition'],
+		$data['variable_sip_hangup_disposition'],
+		$data['variable_sip_term_cause'],
+		$data['variable_hangup_cause'],
+		urldecode($data['variable_start_stamp']) . '-05',
+		isset($data['variable_answer_stamp'])?'\''. urldecode($data['variable_answer_stamp']) . '-05\'':'null',
+		urldecode($data['variable_progress_media_stamp']) . '-05',
+		urldecode($data['variable_end_stamp']) . '-05',
+		$data['variable_duration'],
+		$data['variable_billsec'],
+		$data['variable_progress_mediasec'],
+		$data['variable_billsec'],
+		"0",
+		"0",
+		"0"
+		);
+	
+		$inserted = $dbh->exec($insert);
+		if ($inserted < 1){
+			die($db->errorInfo());
+		}
 	}
 }
 ?> 
