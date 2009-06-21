730b2a7efb9e47486cbd479ad1932f70791988e1
Leon de Rooij <leon@scarlet-internet.nl>
2009-06-21 16:23:10 +0000

    Modified test_data.sql to reflect change in structure
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/scripts/contrib@13886 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- ledr/mod_xml_odbc/test_data.sql
+++ ledr/mod_xml_odbc/test_data.sql
@@ -3,7 +3,6 @@ INSERT INTO `dir_domain_params` (`id`, `dir_domain_id`, `name`, `value`) VALUES 
 INSERT INTO `dir_domain_variables` (`id`, `dir_domain_id`, `name`, `value`) VALUES (1, 1, 'dom_variable_name', 'dom_variable_value');
 INSERT INTO `dir_groups` (`id`, `name`) VALUES (1, 'prepay'), (2, 'annoying_customers');
 INSERT INTO `dir_groups_users` (`dir_group_id`, `dir_user_id`) VALUES (1, 1), (2, 1);
-INSERT INTO `dir_users` (`id`, `dir_domain_id`) VALUES (1, 1);
-INSERT INTO `dir_user_attrs` (`id`, `dir_user_id`, `name`, `value`) VALUES (1, 1, 'id', 'someuser'), (2, 1, 'mailbox', '1234');
+INSERT INTO `dir_users` (`id`, `dir_domain_id`, `username`, `cidr`, `mailbox`, `number-alias`) VALUES (1, 1, 'someuser', NULL, 'leon@toyos.nl', '0031320227470');
 INSERT INTO `dir_user_params` (`id`, `dir_user_id`, `name`, `value`) VALUES (1, 1, 'password', 'topsecret'), (2, 1, 'vm-password', '0000');
 INSERT INTO `dir_user_variables` (`id`, `dir_user_id`, `name`, `value`) VALUES (1, 1, 'accountcode', '1234'), (2, 1, 'user_context', 'default'), (3, 1, 'vm_extension', '1234');
