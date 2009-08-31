6ae79723d3d6d6f3810a0bbee3debca81997f61f
Diego Viola <diego.viola@gmail.com>
2009-08-31 06:02:13 +0000

    callcard is enough
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@14690 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/callcard/rate.rb
+++ diegoviola/ruby/callcard/rate.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'fastercsv'
 require 'sequel'
 
-DB = Sequel.connect('mysql://root@192.168.0.2/callcard_development')
+DB = Sequel.connect('mysql://root@192.168.0.2/callcard')
 
 FasterCSV.foreach('outbound_rates.csv') do |row|
   DB[:destinations].insert(:country => row[0], :prefix => row[1], :rate => row[4])
