894b3026e016d24dba51ffb88d15a33bb9fefdb6
Diego Viola <diego.viola@gmail.com>
2009-09-05 19:30:18 +0000

    switched to standard csv library
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@14775 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/callcard/load_rates.rb
+++ diegoviola/ruby/callcard/load_rates.rb
@@ -1,10 +1,12 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
 require 'rubygems'
-require 'fastercsv'
-require 'database.rb'
+require 'csv'
+require 'sequel'
 
-FasterCSV.foreach('outbound_rates.csv') do |row|
+DB = Sequel.connect('mysql://root@localhost/callcard')
+
+CSV.foreach('outbound_rates.csv') do |row|
   DB[:rates].insert(:destination => row[0], :prefix => row[1], :first_increment => row[2], :second_increment => row[3], :rate => row[4])
 end
 
