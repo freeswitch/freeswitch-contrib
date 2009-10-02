0dc7bdcf8dd978839f3ac1aae3ad7ea313552828
Chad Phillips <chad@apartmentlines.com>
2009-10-02 17:09:02 +0000

    support functions for database operations in Lua.  currently includes simple escaping functions for MySQL and Postgres database queries -- good to use with LuaSQL's ODBC driver, for example...
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@15053 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ hunmonk/lua/support_functions_database.lua
@@ -0,0 +1,28 @@
+--[[
+  Helper functions for database operations.
+]]
+
+--[[
+  Escapes special characters for the MySQL database.
+]]
+function mysql_escape(string_to_escape)
+  -- Single quote, double quote, backspace.
+  escaped_string, replacements = string.gsub(string_to_escape, "(['\"\\])", function(x) return "\\" .. x end)
+  -- Null byte.
+  escaped_string, replacements = string.gsub(escaped_string, "%z", "\\0")
+
+  return escaped_string
+end
+
+--[[
+  Escapes special characters for the Postgres database.
+]]
+function pgsql_escape(string_to_escape)
+  -- Single quote..
+  escaped_string, replacements = string.gsub(string_to_escape, "'", "''")
+  -- Null byte.
+  escaped_string, replacements = string.gsub(escaped_string, "%z", "")
+
+  return escaped_string
+end
+
