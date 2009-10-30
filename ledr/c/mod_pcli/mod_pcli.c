b68667f329fe62fcb09ba62f9874c881b8c0e8aa
Leon de Rooij <leon@scarlet-internet.nl>
2009-10-30 15:40:16 +0000

    sent packet, but header is the wrong way around.. arf
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@15290 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- ledr/c/mod_pcli/mod_pcli.c
+++ ledr/c/mod_pcli/mod_pcli.c
@@ -26,32 +26,23 @@
  * Leon de Rooij <leon@toyos.nl>
  *
  *
- * mod_pcli.c -- Packet Cable Lawful Intercept
+ * mod_pcli.c -- PacketCable Lawful Intercept
  *
  */
 #include <switch.h>
 
-#define PCLI_HEADER_LEN 32
-#define PCLI_HEADER_LEN_DIRECTION 2
-#define PCLI_HEADER_LEN_CALL_ID 10
-#define PCLI_HEADER_LEN_SWITCH_ID 4
-#define PCLI_HEADER_LEN_INI_ID 16
+SWITCH_MODULE_LOAD_FUNCTION(mod_pcli_load);
+SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_pcli_shutdown);
+SWITCH_MODULE_DEFINITION(mod_pcli, mod_pcli_load, mod_pcli_shutdown, NULL);
 
+SWITCH_STANDARD_APP(pcli_start_function);
 
-/*
 typedef enum {
-	MEDIA_DIRECTION_UNKNOWN,
-	MEDIA_DIRECTION_FROM_TARGET,
-	MEDIA_DIRECTION_TO_TARGET,
-	MEDIA_DIRECTION_MONO_MODE
+	PCLI_MEDIA_DIRECTION_UNKNOWN,     /* 0 */
+	PCLI_MEDIA_DIRECTION_FROM_TARGET, /* 1 */
+	PCLI_MEDIA_DIRECTION_TO_TARGET,   /* 2 */
+	PCLI_MEDIA_DIRECTION_MONO_MODE    /* 3 */
 } pcli_media_direction_t;
-*/
-
-SWITCH_MODULE_LOAD_FUNCTION(mod_li_load);
-SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_li_shutdown);
-SWITCH_MODULE_DEFINITION(mod_li, mod_li_load, mod_li_shutdown, NULL);
-
-SWITCH_STANDARD_APP(li_start_function);
 
 static struct {
 	switch_memory_pool_t *pool;
@@ -61,12 +52,15 @@ static struct {
 	char *remote_addr;
 	int remote_port;
 	switch_socket_t *socket;
-} li_globals;
+} pcli_globals;
+
+// temporary declaration here.. remove after testing is done !!!!
+static switch_status_t gen_pcli_header(uint32_t *pcli_header, pcli_media_direction_t media_direction, uint16_t call_id, uint8_t switch_id, uint16_t ini_id);
 
 typedef struct {
 	switch_core_session_t *session;
 	char *ini_id;
-} li_session_helper_t;
+} pcli_session_helper_t;
 
 /* config item validations */
 static switch_xml_config_int_options_t config_opt_valid_switch_id = { SWITCH_TRUE, 0, SWITCH_TRUE, 15 };
@@ -75,27 +69,28 @@ static switch_xml_config_int_options_t config_opt_valid_port = { SWITCH_TRUE, 0,
 
 /* config items */
 static switch_xml_config_item_t instructions[] = {
-	SWITCH_CONFIG_ITEM("switch_id", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &li_globals.switch_id,
+	SWITCH_CONFIG_ITEM("switch_id", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &pcli_globals.switch_id,
 		(void*)0, &config_opt_valid_switch_id, NULL, NULL),
-	SWITCH_CONFIG_ITEM("local_addr", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &li_globals.local_addr,
+	SWITCH_CONFIG_ITEM("local_addr", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &pcli_globals.local_addr,
 		"localhost", &config_opt_valid_addr, NULL, NULL),
-	SWITCH_CONFIG_ITEM("local_port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &li_globals.local_port,
+	SWITCH_CONFIG_ITEM("local_port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &pcli_globals.local_port,
 		(void*)10741, &config_opt_valid_port, NULL, NULL),
-	SWITCH_CONFIG_ITEM("remote_addr", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &li_globals.remote_addr,
+	SWITCH_CONFIG_ITEM("remote_addr", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &pcli_globals.remote_addr,
 		"localhost", &config_opt_valid_addr, NULL, NULL),
-	SWITCH_CONFIG_ITEM("remote_port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &li_globals.remote_port,
+	SWITCH_CONFIG_ITEM("remote_port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &pcli_globals.remote_port,
 		(void*)10741, &config_opt_valid_port, NULL, NULL),
   SWITCH_CONFIG_ITEM_END()
 };
 
+/* called at startup and reload of the module */
 static switch_status_t do_config(switch_bool_t reload)
 {
 	switch_socket_t *socket = NULL;
 	switch_sockaddr_t *local_sockaddr;
 	switch_sockaddr_t *remote_sockaddr;
 
-	if (switch_xml_config_parse_module_settings("li.conf", reload, instructions) != SWITCH_STATUS_SUCCESS) {
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Could not open li.conf\n");
+	if (switch_xml_config_parse_module_settings("pcli.conf", reload, instructions) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Could not open pcli.conf\n");
 		return SWITCH_STATUS_FALSE;
 	}
 
@@ -104,12 +99,12 @@ static switch_status_t do_config(switch_bool_t reload)
 
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Trying to create a udp socket !!\n");
 
-		if (switch_sockaddr_info_get(&local_sockaddr, li_globals.local_addr, SWITCH_UNSPEC, li_globals.local_port, 0, li_globals.pool) != SWITCH_STATUS_SUCCESS || !local_sockaddr) {
+		if (switch_sockaddr_info_get(&local_sockaddr, pcli_globals.local_addr, SWITCH_UNSPEC, pcli_globals.local_port, 0, pcli_globals.pool) != SWITCH_STATUS_SUCCESS || !local_sockaddr) {
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Local Address Error!\n");
 			return SWITCH_STATUS_FALSE;
 		}
 
-		if (switch_socket_create(&socket, switch_sockaddr_get_family(local_sockaddr), SOCK_DGRAM, 0, li_globals.pool) != SWITCH_STATUS_SUCCESS) {
+		if (switch_socket_create(&socket, switch_sockaddr_get_family(local_sockaddr), SOCK_DGRAM, 0, pcli_globals.pool) != SWITCH_STATUS_SUCCESS) {
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Socket Error!\n");
 			return SWITCH_STATUS_FALSE;
 		}
@@ -124,16 +119,35 @@ static switch_status_t do_config(switch_bool_t reload)
 			return SWITCH_STATUS_FALSE;
 		}
 
-		li_globals.socket = socket;
+		pcli_globals.socket = socket;
 
 		/* TEST SENDING A PACKET */
-		if (switch_sockaddr_info_get(&remote_sockaddr, li_globals.remote_addr, SWITCH_UNSPEC, li_globals.remote_port, 0, li_globals.pool) != SWITCH_STATUS_SUCCESS || !remote_sockaddr) {
+		if (switch_sockaddr_info_get(&remote_sockaddr, pcli_globals.remote_addr, SWITCH_UNSPEC, pcli_globals.remote_port, 0, pcli_globals.pool) != SWITCH_STATUS_SUCCESS || !remote_sockaddr) {
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_CRIT, "Remote Address Error!\n");
 			return SWITCH_STATUS_FALSE;
 		}
 
-		switch_size_t len = sizeof("X");
-		switch_socket_sendto(li_globals.socket, remote_sockaddr, 0, "X", &len);
+		/* create a pcli header */
+		uint32_t pcli_header;
+		pcli_media_direction_t media_direction = PCLI_MEDIA_DIRECTION_FROM_TARGET;
+		uint16_t call_id = 123;
+		uint8_t switch_id = 12;
+		uint16_t ini_id = 10741;
+		gen_pcli_header(&pcli_header, media_direction, call_id, switch_id, ini_id);
+
+		/* create a pcli body */
+		char *pcli_body = "ABCD";
+
+		/* create a full packet */
+		char *packet;
+		packet = malloc(sizeof(pcli_header) + sizeof(pcli_body));
+		memcpy(packet, &pcli_header, sizeof(pcli_header));
+		memcpy(packet + sizeof(pcli_header), pcli_body, sizeof(pcli_body));
+
+		size_t packetsize;
+		packetsize = sizeof(packet);
+
+		switch_socket_sendto(pcli_globals.socket, remote_sockaddr, 0, packet, &packetsize);
 
 	}
 
@@ -146,8 +160,8 @@ static void reload_event_handler(switch_event_t *event)
   do_config(SWITCH_TRUE);
 }
 
-//static switch_status_t gen_pcli_header(uint32_t *pcli_heqader, pcli_media_direction_t media_direction, int call_id, int switch_id, int ini_id)
-static switch_status_t gen_pcli_header(uint32_t *pcli_header, uint8_t media_direction, uint16_t call_id, uint8_t switch_id, uint16_t ini_id)
+/* generate a PacketCable Lawful Intercept header, that can be prepended to an RTP packet */
+static switch_status_t gen_pcli_header(uint32_t *pcli_header, pcli_media_direction_t media_direction, uint16_t call_id, uint8_t switch_id, uint16_t ini_id)
 {
 	*pcli_header = 0;
 
@@ -178,12 +192,9 @@ static switch_status_t gen_pcli_header(uint32_t *pcli_header, uint8_t media_dire
 	return SWITCH_STATUS_SUCCESS;
 }
 
-/* necessary for hooking to SWITCH_EVENT_RELOADXML */
-static switch_event_node_t *NODE = NULL;
-
-static switch_bool_t li_callback(switch_media_bug_t *bug, void *user_data, switch_abc_type_t type)
+static switch_bool_t pcli_callback(switch_media_bug_t *bug, void *user_data, switch_abc_type_t type)
 {
-//	li_session_helper_t *li_session_helper = (li_session_helper_t *) user_data;
+//	pcli_session_helper_t *pcli_session_helper = (pcli_session_helper_t *) user_data;
 
 	switch (type) {
 	case SWITCH_ABC_TYPE_INIT:
@@ -232,12 +243,15 @@ switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, ">>> [%lu]\n", (unsigne
 	return SWITCH_TRUE;
 }
 
-SWITCH_STANDARD_APP(li_start_function)
+/* necessary for hooking to SWITCH_EVENT_RELOADXML */
+static switch_event_node_t *NODE = NULL;
+
+SWITCH_STANDARD_APP(pcli_start_function)
 {
     switch_media_bug_t *bug;
     switch_status_t status;
     switch_channel_t *channel;
-    li_session_helper_t *li_session_helper;
+    pcli_session_helper_t *pcli_session_helper;
 		const char *p;
 
     if (session == NULL)
@@ -246,12 +260,12 @@ SWITCH_STANDARD_APP(li_start_function)
     channel = switch_core_session_get_channel(session);
 
     /* Is this channel already set? */
-    bug = (switch_media_bug_t *) switch_channel_get_private(channel, "_li_");
+    bug = (switch_media_bug_t *) switch_channel_get_private(channel, "_pcli_");
     /* If yes */
     if (bug != NULL) {
         /* If we have a stop remove audio bug */
         if (strcasecmp(data, "stop") == 0) {
-            switch_channel_set_private(channel, "_li_", NULL);
+            switch_channel_set_private(channel, "_pcli_", NULL);
             switch_core_media_bug_remove(session, &bug);
             return;
         }
@@ -262,43 +276,43 @@ SWITCH_STANDARD_APP(li_start_function)
         return;
     }
 
-		/* create a new li_session_helper */
-    li_session_helper = (li_session_helper_t *) switch_core_session_alloc(session, sizeof(li_session_helper_t));
+		/* create a new pcli_session_helper */
+    pcli_session_helper = (pcli_session_helper_t *) switch_core_session_alloc(session, sizeof(pcli_session_helper_t));
 
-		/* make session available from li_session_helper */
-    li_session_helper->session = session;
+		/* make session available from pcli_session_helper */
+    pcli_session_helper->session = session;
 
-		/* get LI_ID and store in li_session_helper */
+		/* get LI_ID and store in pcli_session_helper */
 		if ((p = switch_channel_get_variable(channel, "LI_ID")) && switch_true(p)) {
-			li_session_helper->ini_id = switch_core_session_strdup(session, p);
+			pcli_session_helper->ini_id = switch_core_session_strdup(session, p);
 		} else {
 			switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "No LI_ID was set!\n");
 			return;
 		}
 
 		/* add the bug */
-    //status = switch_core_media_bug_add(session, li_callback, li_session_helper, 0, SMBF_BOTH, &bug);
-    //status = switch_core_media_bug_add(session, li_callback, li_session_helper, 0, SMBF_READ_STREAM | SMBF_WRITE_STREAM, &bug);
-    status = switch_core_media_bug_add(session, li_callback, li_session_helper, 0, SMBF_READ_REPLACE | SMBF_WRITE_REPLACE, &bug);
-    //status = switch_core_media_bug_add(session, li_callback, li_session_helper, 0, SMBF_READ_REPLACE, &bug);
+    //status = switch_core_media_bug_add(session, pcli_callback, pcli_session_helper, 0, SMBF_BOTH, &bug);
+    //status = switch_core_media_bug_add(session, pcli_callback, pcli_session_helper, 0, SMBF_READ_STREAM | SMBF_WRITE_STREAM, &bug);
+    status = switch_core_media_bug_add(session, pcli_callback, pcli_session_helper, 0, SMBF_READ_REPLACE | SMBF_WRITE_REPLACE, &bug);
+    //status = switch_core_media_bug_add(session, pcli_callback, pcli_session_helper, 0, SMBF_READ_REPLACE, &bug);
 
     if (status != SWITCH_STATUS_SUCCESS) {
         switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Failure hooking to stream\n");
         return;
     }
 
-    switch_channel_set_private(channel, "_li_", bug);
+    switch_channel_set_private(channel, "_pcli_", bug);
 }
 
-SWITCH_MODULE_LOAD_FUNCTION(mod_li_load)
+SWITCH_MODULE_LOAD_FUNCTION(mod_pcli_load)
 {
     switch_application_interface_t *app_interface;
 
-		/* initialize li_globals struct */
-		memset(&li_globals, 0, sizeof(li_globals));
+		/* initialize pcli_globals struct */
+		memset(&pcli_globals, 0, sizeof(pcli_globals));
 
-		/* make the pool available from li_globals */
-		li_globals.pool = pool;
+		/* make the pool available from pcli_globals */
+		pcli_globals.pool = pool;
 
 		/* do config */
 		do_config(SWITCH_FALSE);
@@ -306,8 +320,8 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_li_load)
     /* connect my internal structure to the blank pointer passed to me */
     *module_interface = switch_loadable_module_create_module_interface(pool, modname);
 
-		/* make the li application available to the system */
-    SWITCH_ADD_APP(app_interface, "li", "Lawful Intercept", "Lawful Intercept", li_start_function, "<start>", SAF_NONE);
+		/* make the pcli application available to the system */
+    SWITCH_ADD_APP(app_interface, "pcli", "PacketCable Lawful Intercept", "PacketCable Lawful Intercept", pcli_start_function, "<start>", SAF_NONE);
 
 		/* subscribe to reloadxml event, and hook it to reload_event_handler */
 		if ((switch_event_bind_removable(modname, SWITCH_EVENT_RELOADXML, NULL, reload_event_handler, NULL, &NODE) != SWITCH_STATUS_SUCCESS)) {
@@ -323,9 +337,9 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_li_load)
 }
 
 
-SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_li_shutdown)
+SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_pcli_shutdown)
 {
-    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Lawful Intercept disabled\n");
+    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "PacketCable Lawful Intercept disabled\n");
 
 /* TODO DESTROY THE SOCKET HERE ! */
 
