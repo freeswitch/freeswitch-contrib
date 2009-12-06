e585fb9a02c2d45dadd8f857776cdf21194df3ef
Diego Viola <diego.viola@gmail.com>
2009-12-06 15:21:01 +0000

    convert app to ramaze
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@15804 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/misc/bridge.rb
+++ diegoviola/ruby/misc/bridge.rb
@@ -1,17 +1,23 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
-require 'erb'
+require 'ramaze'
 require 'fsr'
 
 FSR.load_all_commands
 
-get '/' do
-  erb "Phone number: <form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
+class Main < Ramaze::Controller
+  engine :ERB
+
+  def index
+    "Phone number: <form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
+  end
+
+  def originate
+    sock = FSR::CommandSocket.new
+    sock.originate(:target => "sofia/gateway/flowroute/#{params[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
+    redirect '/'
+  end
 end
 
-post '/originate' do
-  sock = FSR::CommandSocket.new
-  sock.originate(:target => "sofia/gateway/flowroute/#{params[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
-  redirect '/'
-end
+Ramaze.start
+
