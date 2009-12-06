3c3bcc743c624804ea7ee5242ded5930aa5a8375
Diego Viola <diego.viola@gmail.com>
2009-12-06 07:57:39 +0000

    some minor changes
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@15803 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/misc/bridge.rb
+++ diegoviola/ruby/misc/bridge.rb
@@ -7,12 +7,11 @@ require 'fsr'
 FSR.load_all_commands
 
 get '/' do
-  erb "<form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
+  erb "Phone number: <form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
 end
 
 post '/originate' do
   sock = FSR::CommandSocket.new
-  sock.originate(:target => "sofia/gateway/gizmo/#{params[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
-  #sock.originate(:target => "sofia/internal/#{params[:phone_number]}@65.208.88.11", :endpoint => FSR::App::ExecuteApp.new("playback", "/tmp/200911271756500.fondo4.wav")).run
+  sock.originate(:target => "sofia/gateway/flowroute/#{params[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
   redirect '/'
 end
