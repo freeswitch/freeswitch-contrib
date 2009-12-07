97fbfafdcfa75e236d99be0fdf71f9100e120abd
Diego Viola <diego.viola@gmail.com>
2009-12-07 06:40:20 +0000

    sinatra is good enough
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@15810 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/misc/bridge.rb
+++ diegoviola/ruby/misc/bridge.rb
@@ -1,23 +1,17 @@
 #!/usr/bin/env ruby
 
-require 'ramaze'
+require 'sinatra'
+require 'erb'
 require 'fsr'
 
 FSR.load_all_commands
 
-class Main < Ramaze::Controller
-  engine :ERB
-
-  def index
-    "Phone number: <form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
-  end
-
-  def originate
-    sock = FSR::CommandSocket.new
-    sock.originate(:target => "sofia/gateway/flowroute/#{request[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
-    redirect '/'
-  end
+get '/' do
+  erb "Phone number: <form action=\"/originate\" method=\"post\"><input type=\"text\" name=\"phone_number\"><input type=\"submit\"></form>"
 end
 
-Ramaze.start
-
+post '/originate' do
+  sock = FSR::CommandSocket.new
+  sock.originate(:target => "sofia/gateway/flowroute/#{params[:phone_number]}", :endpoint => FSR::App::Conference.new("3030", "default")).run
+  redirect '/'
+end
