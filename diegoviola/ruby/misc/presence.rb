3d9538b38be695810e0b85ea8044c2f921108b9b
Diego Viola <diego.viola@gmail.com>
2010-03-16 14:56:56 +0000

    some minor changes
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk/contrib@17008 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- diegoviola/ruby/misc/presence.rb
+++ diegoviola/ruby/misc/presence.rb
@@ -4,11 +4,11 @@ require 'rubygems'
 require 'fsr'
 require "fsr/listener/inbound"
 
-def custom_channel_hangup_handler(event)
+def custom_channel_presence_handler(event)
   FSR::Log.info "From: #{event.content[:from]} - #{event.content[:status]}"
 end
 
-FSL::Inbound.add_event_hook(:PRESENCE_IN) {|event| custom_channel_hangup_handler(event) }
-FSL::Inbound.add_event_hook(:PRESENCE_OUT) {|event| custom_channel_hangup_handler(event) }
+FSL::Inbound.add_event_hook(:PRESENCE_IN) {|event| custom_channel_presence_handler(event) }
+FSL::Inbound.add_event_hook(:PRESENCE_OUT) {|event| custom_channel_presence_handler(event) }
 
 FSR.start_ies!(FSL::Inbound, :host => "localhost", :port => 8021)
