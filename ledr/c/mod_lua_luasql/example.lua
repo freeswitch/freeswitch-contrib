5d58b895258c188676bfb92299be47551a085bfc
Leon de Rooij <leon@toyos.nl>
2010-08-31 21:15:57 +0200

    can't think of anything more to add to example

--- ledr/c/mod_lua_luasql/example.lua
+++ ledr/c/mod_lua_luasql/example.lua
@@ -1,15 +1,17 @@
+-- example.lua - just put this script in scripts/ and run it from you fs_cli
+
 local env = assert(luasql.scdb()) -- same as regular luasql
 local con = assert(env:connect("dsn","user","pass")) -- same as regular luasql
 
 local res = assert(con:exec2table("select 1 as foo, 2 as bar")) -- all your rows in a single table
 
---[[ so, now res will look like this :
+--[[ now res will contain this :
 {
   [1] = {
     ["foo"] = "1";
     ["bar"] = "2";
   };
-}
+};
 ]]
  
 for i = 1, table.getn(res) do -- loop through rows
@@ -23,3 +25,11 @@ con:close(); -- optional, it is also closed at first gc run,
  -- although it does release the dbh for re-use by another thread so can be efficient to do it early
 
 env:close(); -- optional, (same reason) just don't close your env before closing the con !
+
+--[[ output on your fs_cli should be :
+
+freeswitch@internal> lua example.lua
+                      bar : 2
+                      foo : 1
+
+]]
