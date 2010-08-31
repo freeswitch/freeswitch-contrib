7b36aeb674e77254fde8952d1a8a0069548fa9b7
Leon de Rooij <leon@toyos.nl>
2010-08-31 20:45:23 +0200

    added comments to example

--- ledr/c/mod_lua_luasql/example.lua
+++ ledr/c/mod_lua_luasql/example.lua
@@ -1,13 +1,16 @@
-local env = assert(luasql.scdb()) 
-local con = assert(env:connect("dsn","user","pass")) 
-local res = assert(con:exec2table("select 1 as foo, 2 as bar")) 
+local env = assert(luasql.scdb()) -- same as regular luasql
+local con = assert(env:connect("dsn","user","pass")) -- same as regular luasql
+
+local res = assert(con:exec2table("select 1 as foo, 2 as bar")) -- all your rows in a single table
  
-for i = 1, table.getn(res) do 
-  for key, val in pairs(res[i]) do 
+for i = 1, table.getn(res) do -- loop through rows
+  for key, val in pairs(res[i]) do -- loop through columns
     stream:write(string.format("%25s : %s\n", key, val)) 
   end 
   stream:write("\n") 
 end 
  
-con:close(); 
-env:close();
+con:close(); -- optional, it is also closed at first gc run,
+ -- although it does release the dbh for re-use by another thread so can be efficient to do it early
+
+env:close(); -- optional, (same reason) just don't close your env before closing the con !
