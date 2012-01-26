b411a5296a8bc46fe3e8c2132cb7c0a432d6bfe4
Raymond Chandler <intralanman@gmail.com>
2012-01-26 13:43:20 -0500

    refactor a bit

--- intralanman/PHP/phttapi/example/main_menu.php
+++ intralanman/PHP/phttapi/example/main_menu.php
@@ -1,58 +1,61 @@
 <?php
 
 if ( array_key_exists( 'option', $_REQUEST ) ) {
-  $option = $_REQUEST['option'];
+	$option = $_REQUEST ['option'];
 } else {
-  $option = '';
+	$option = '';
 }
 
 if ( $option ) {
-  switch ( $option ) {
-  case '1':
-    $menu_ivr_path = 'disa';
-    break;
-  case '2':
-    $menu_ivr_path = 'record';
-    break;
-    //  case '':
-    //    $menu_ivr_path = '';
-    //    break;
-  }
+	switch ( $option ) {
+		case '1' :
+			$menu_ivr_path = 'disa';
+			break;
+		case '2' :
+			$menu_ivr_path = 'record';
+			break;
+		// case '':
+		// $menu_ivr_path = '';
+		// break;
+	}
 
-  $api = new phttapi();
+	$api = new phttapi();
 
-  $api->start_variables();
-  $api->add_variable( 'ivr_path', $menu_ivr_path );
-  $api->end_variables();
+	$api->start_variables();
+	$api->add_variable( 'ivr_path', $menu_ivr_path );
+	$api->end_variables();
 
-  $api->start_work();
+	$api->start_work();
 
-  $var = new phttapi_getVariable( 'ivr_path' );
-  $var->permanent( 'true' );
-  $api->add_action( $var );
+	$var = new phttapi_getVariable( 'ivr_path' );
+	$var->permanent( 'true' );
+	$api->add_action( $var );
 
-  $api->end_work();
-  print $api->output();
+	$api->end_work();
+	print $api->output();
 
 } else {
-  $api = new phttapi();
-  $api->start_work();
+	$api = new phttapi();
+	$api->start_work();
 
-  $bind1 = new phttapi_action_binding( '1' );
-  $bind2 = new phttapi_action_binding( '2' );
+	$pause = new phttapi_pause();
+	$pause->milliseconds( 1500 );
 
-  $play = new phttapi_speak( "to make an outbound call, press 1,, to record a file, press 2,, " );
-  $play->engine( 'flite' );
-  $play->voice( 'slt' );
-  $play->name( 'option' );
-  $play->input_timeout( '5000' );
-  $play->add_binding($bind1);
-  $play->add_binding($bind2);
+	$bind1 = new phttapi_action_binding( '1' );
+	$bind1->strip( '#' );
+	$bind2 = new phttapi_action_binding( '2' );
 
-  $api->add_action( $play );
-  $api->end_work();
-  print $api->output();
+	$play = new phttapi_google_tts();
+	$play->text( "to make an outbound call, press 1,, to record a file, press 2" );
+	$play->name( 'option' );
+	$play->input_timeout( '5000' );
+	$play->add_binding( $bind1 );
+	$play->add_binding( $bind2 );
 
+	$api->add_action( $pause );
+	$api->add_action( $play );
+	$api->end_work();
+	print $api->output();
 }
 
 
